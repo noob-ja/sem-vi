c8f0199f3cdbc22e1f4df092ac1c8f4e7627d5b0
noob-ja <jiaaun96@hotmail.my>
2018-02-27 11:39:38 +0800

    interpolation

--- WID3004 Numerical Methods/Interpolation/LagrangePolynomialInterpolation.java
+++ WID3004 Numerical Methods/Interpolation/LagrangePolynomialInterpolation.java
@@ -1,17 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package interpolation;
 
 /**
  *
  * @author JA
  */
-public class LinearInterpolation {
+public class LagrangePolynomialInterpolation {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double X[] = {1,2,3,4};
-        double Y[] = {1,4,9,16};
-        double x = 10.0;
+        double X[] = {1,2,3,4,5};
+        double Y[] = {1,16,81,256,625};
+        double x = 19.5;
         System.out.println(polynomial(x,X,Y));
     }
     
