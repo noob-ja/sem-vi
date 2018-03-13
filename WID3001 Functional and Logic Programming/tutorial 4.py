61c0d51d3305fe1dc6d152f8b639e58ddbbf7cc3
noob-ja <jiaaun96@hotmail.my>
2018-03-13 18:17:08 +0800

    flp

--- WID3001 Functional and Logic Programming/tutorial 4.py
+++ WID3001 Functional and Logic Programming/tutorial 4.py
@@ -7,26 +7,25 @@ Created on Mon Mar  5 16:24:09 2018
 
 #Q1
 inStr = "ASCII stands for American Standard Code for Information Interchange. Computers can only understand numbers, so an ASCII code is the numerical representation of a character such as 'a' or '@' or an action of some sort. ASCII was developed a long time ago and now the non-printing characters are rarely used for their original purpose. Below is the ASCII character table and this includes descriptions of the first 32 non-printing characters. ASCII was actually designed for use with teletypes and so the descriptions are somewhat obscure. If someone says they want your CV however in ASCII format, all this means is they want 'plain' text with no formatting such as tabs, bold or underscoring - the raw format that any computer can understand. This is usually so they can easily import the file into their own applications without issues. Notepad.exe creates ASCII text, or in MS Word you can save a file as 'text only'"
-inStr = "artificial"
+# inStr = "artificial"
 def countChar(string):
-    #alphabets = {string.lower()[x]:0 for x in range(len(string))}
+	#add each character to a dictionary, then pass the dictionary as the one to loop through	
     return {c:string.lower().count(c) for c in {string.lower()[x]:0 for x in range(len(string))}.keys()}
 print(countChar(inStr))
-def countChar2(string):
-    return {x:string.lower().count(x) for x in [chr(x) for x in range(ord('a'),ord('z')+1)] if string.lower().count(x)>0}
-
-def countChar3(string):
-    return {string.lower()[x]:string.lower().count(string[x]) for x in range(len(string))}
-
+# def countChar2(string):
+#     return {x:string.lower().count(x) for x in [chr(x) for x in range(ord('a'),ord('z')+1)] if string.lower().count(x)>0}
+# def countChar3(string):
+#     return {string.lower()[x]:string.lower().count(string[x]) for x in range(len(string))}
 #print(countChar3(inStr))
 
 #Q2
 import string as strrr
 def removePunct(string):
-  return ''.join(c for c in string if c not in strrr.punctuation)
-#print(removePunct(inStr))
+	#join an empty string with each character return if is not a punctuation
+  	return ''.join(c for c in string if c not in strrr.punctuation)
+print(removePunct(inStr))
     
 def normalize(string):
-  return removePunct(string).lower().split()
-
+	#return removePunct(string) with lower and split
+  	return removePunct(string).lower().split()
 print(normalize('hello,..,WORLD.'))
--- WID3001 Functional and Logic Programming/tutorial 5.py
+++ WID3001 Functional and Logic Programming/tutorial 5.py
@@ -4,8 +4,88 @@ Created on Mon Mar 12 15:22:18 2018
 
 @author: user
 """
-def outer(a):
-    return lambda x: a+x
+#q1
+def removeDup(inlist):
+    return list(set(inlist))
+print(removeDup('artificial'))
 
-output = outer(1)
-print(output(3))
+#q2
+import string
+def countType(instr):
+    instr = [x for x in instr if x in string.ascii_lowercase]
+    dicti = {'vowel':0,'consonant':0}
+    for x in instr:
+        if x in 'aeiou':
+            dicti['vowel']+=1
+        else:
+            dicti['consonant']+=1
+    return dicti
+print(countType('artificial'))
+def countType2(instr):
+    temp = list(map(lambda x: 1 if x in 'aeiou' else 0,[x for x in instr.lower() if x in string.ascii_lowercase]))
+    return {'vowel':temp.count(1), 'consonant':temp.count(0)}
+print(countType2('artificial'))
+    
+#q3
+def vowel(instr):
+    return {x:instr.lower().count(x) for x in 'aeiou' if instr.lower().count(x)>0}
+print(vowel('artificial'))
+
+#q4
+import math
+def cylinder(radius):
+    def volume(height):
+        return math.pi * radius * radius * height
+    return volume
+cylin = cylinder(5)
+cylin = cylin(10)
+print(cylin)
+
+#TUTORIAL
+#q1
+import string
+def isPalindrome(instr):
+    instr = ''.join(c for c in (''.join(c for c in instr if c not in string.punctuation).lower().split()))
+    return instr[::-1]==instr
+instr = ('A man, a plan, a canal: Panama')
+print(isPalindrome(instr))
+
+#q2
+from functools import reduce
+inlist = ['amore','roma','byebuii']
+
+def sort(a,inlist):
+    funcs = {'len': sorted(inlist,key=len), 'lchar': sorted(inlist,key=(lambda x:x[-1])), 'nvoweldesc': sorted(inlist,key=(lambda x: reduce(lambda x,y:x+y, [x.count(c) for c in 'aeiou'])),reverse=True)}
+    return funcs[a] if a in funcs.keys() else 'error'
+print(sort('len',inlist))
+
+'''
+def sortByLength(inlist):
+    return sorted(inlist,key=len)
+print(sortByLength(inlist))
+
+def sortByLastCharacter(inlist):
+    return sorted(inlist,key=(lambda x: x[-1]))
+print(sortByLastCharacter(inlist))
+
+def sortByNumberOfVowelDesc(inlist):
+    return sorted(inlist,key=(lambda x: reduce(lambda x,y:x+y, [x.count(c) for c in 'aeiou'])),reverse=True)
+print(sortByNumberOfVowelDesc(inlist))
+'''
+
+from functools import reduce
+#q3
+instr = "hello hello hello byebye lol"
+dicti = {'countWord': lambda instr:{x:instr.count(x) for x in {x:0 for x in instr.split()}.keys()}}
+print(dicti['countWord'](instr))
+
+#q4
+def factorial(n):
+  return reduce(lambda x,y: x*y, range(1,n+1))
+print(factorial(5))
+
+#q5
+def listtodict(x):
+  return {x[0]: listtodict(x[1:]) if len(x) > 1 else {}}
+listToDict = listtodict
+print(listToDict(range(10)))
