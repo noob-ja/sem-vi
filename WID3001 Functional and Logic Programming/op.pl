738c0e0c6bc7674d3a52b6a6df55cace6bddb0fa
JA <jiaaun96@hotmail.my>
2018-04-09 16:57:02 +0800

    Update op.pl

--- WID3001 Functional and Logic Programming/op.pl
+++ WID3001 Functional and Logic Programming/op.pl
@@ -1,2 +1,12 @@
 +(-(2,*(1,3)),4)
 -(2,+(*(1,3),4))
+
+:-op(300, xfx, likes).
+:-op(200, xfy, and).
+likes(danial, drawing).
+likes(danial, reading).
+
+danial likes drawing and reading
+likes(danial,and(reading, drawing))
+
+?.danial likes What and reading.
