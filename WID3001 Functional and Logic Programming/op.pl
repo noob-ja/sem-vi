958d6f90f8bf8ee797beb7e06df7bd9990f273e7
JA <jiaaun96@hotmail.my>
2018-04-10 15:09:34 +0800

    Update op.pl

--- WID3001 Functional and Logic Programming/op.pl
+++ WID3001 Functional and Logic Programming/op.pl
@@ -10,3 +10,30 @@ danial likes drawing and reading
 likes(danial,and(reading, drawing))
 
 ?.danial likes What and reading.
+
+:-op(500,yfx,chases).
+dog('dude').
+cat('girl').
+chases(X,Y):- dog(X), cat(Y).
+?.Who chases Whom.
+
+%TUTORIAL
+%Q1
+%ball and marble inside blue_box inside red_box
+:-op(400,xfy,and).
+:-op(600,yfx,inside).
+inside(inside(and(ball,marble),blue_box),red_box).
+?.What and Wut inside What2 inside red_box.
+
+:-op(400,xfy,and).
+:-op(600,xfy,inside).
+inside(and(ball,marble),inside(blue_box,red_box)).
+
+Left-associative is better, more human-like
+
+%Q2
+:-op(500,yfx,was).
+:-op(400,yfx,of).
+:-op(300,fy,the).
+was(zara,of(the(dean),the(faculty))).
+?. Who is the dean of the faculty.
