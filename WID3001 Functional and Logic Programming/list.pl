ed84dd2527e22c3ab7b68991c7346d789212d38e
JA <jiaaun96@hotmail.my>
2018-04-23 16:49:47 +0800

    Update list.pl

--- WID3001 Functional and Logic Programming/list.pl
+++ WID3001 Functional and Logic Programming/list.pl
@@ -22,8 +22,9 @@ insert(X,L,L1):- del(X,L1,L).
 
 %TUTORIAL
 %Q1
-palindrome_date([X,Y,Z]):- split_d(X,X1), split_d(Y,Y1), split_d(Z,Z1), conc(X1,Y1,L), conc(L,Z1,List), write(List) .
-split_d(X,R):- X1 is X mod 10, X2 is (X - X1)/10, R = [X2,X1].
+%palindrome_date([X,Y,Z]):- split_d(X,X1), split_d(Y,Y1), split_d(Z,Z1), conc(X1,Y1,L), conc(L,Z1,List), write(List) .
+%split_d(X,R):- X1 is X mod 10, X2 is (X - X1)/10, R = [X2,X1].
+palindrome_date([X,Y,Z]):- string_chars(X,X1), string_chars(Y,Y1), string_chars(Z,Z1), conc(X1,Y1,L), conc(L,Z1,List), my_reverse(List,Reverse,[]), Reverse == List.
 
 my_reverse([],L,L).
 my_reverse([X|L1],L2,L3):- my_reverse(L1,L2,[X|L3]).
